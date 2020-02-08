374a06ad0e63f59175c72519ba4639fe2bfad4bf
zac@witdelivers.com <zacharyjsanders@gmail.com>
2020-02-08 11:22:57 -0700

    js header fix for desktop display

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,36 +1,28 @@
-<!-- <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-    <button class="hamburger hamburger--vortex" type="button">
-      <span class="hamburger-box">
-        <span class="hamburger-inner"></span>
-      </span>
-    </button>
-  </div>
-</header> -->
-
 <header class="banner">
  <div class="container">
- <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
- data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
- aria-label="Toggle navigation">
- <span class="navbar-toggler-icon"></span>
- </button>
-<a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
- @if (has_nav_menu('primary_navigation'))
- {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav pull-right']) !!}
- @endif
- </div>
- <!-- <form class="form-inline collapse navbar-collapse pull-right">
- <button class="btn btn-outline-success" type="button">Main button</button>
- </form> -->
- </nav>
+   <nav class="navbar navbar-toggleable-lg navbar-light bg-primary">
+    <button
+      class="navbar-toggler hamburger hamburger--vortex"
+      type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+       <span class="hamburger-box">
+         <span class="hamburger-inner"></span>
+       </span>
+    </button>
+  <!-- <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a> -->
+  <div class="collapse navbar-collapse mobileNavigation" id="navbarSupportedContent">
+     @if (has_nav_menu('mobile_navigation'))
+     {!! wp_nav_menu(['theme_location' => 'mobile_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav pull-right']) !!}
+     @endif
+   </div>
+   <div class="test">
+      @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav']) !!}
+      @endif
+    </div>
+  </nav>
  </div>
 </header>
